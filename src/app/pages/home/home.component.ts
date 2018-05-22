f6f4611b366e45f3838f730bcded95c91508ae4c
Victor Jordan <victorjordan95@gmail.com>
2018-05-22 15:27:54 -0300

    removendo arquivos desnecessarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { CarteiraModalComponent } from './carteira-modal/carteira-modal.component';
 
 @Component({
     selector: 'app-home',
@@ -8,16 +7,9 @@ import { CarteiraModalComponent } from './carteira-modal/carteira-modal.componen
 })
 export class HomeComponent implements OnInit {
 
-    @ViewChild(CarteiraModalComponent) modalComponent: CarteiraModalComponent;
-
     constructor() { }
 
     ngOnInit() {
-    }
-
-    showModal() {
-        console.log('entrei no primeiro');
-        this.modalComponent.showModal();
     }
 
 }
