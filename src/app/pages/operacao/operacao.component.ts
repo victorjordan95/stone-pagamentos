3bb3954b06841e570d5ec8e9a134e939b9cc1189
Victor Jordan <victorjordan95@gmail.com>
2018-05-23 21:05:34 -0300

    criacao do floating button com toggle

--- src/app/pages/operacao/operacao.component.ts
+++ src/app/pages/operacao/operacao.component.ts
@@ -1,5 +1,6 @@
 import { OperacaoModalComponent } from './operacao-modal/operacao-modal.component';
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-operacao',
@@ -9,14 +10,39 @@ import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 export class OperacaoComponent implements OnInit {
     @ViewChild(OperacaoModalComponent) modalComponent: OperacaoModalComponent;
 
+    public show = false;
+    public buttonName = 'Show';
+
     constructor() { }
 
     ngOnInit() {
+        $(document).ready(function () {
+            $('#js-operationBtn').click(function () {
+                $('#js-fabOptions').fadeToggle();
+            });
+        });
+
     }
+
 
     showModal() {
         console.log('entrei no primeiro');
         this.modalComponent.showModal();
+    }
+
+    openOptions() {
+        const list = document.getElementById('options');
+    }
+
+    toggle() {
+        this.show = !this.show;
+
+        // CHANGE THE NAME OF THE BUTTON.
+        if (this.show) {
+            this.buttonName = 'Hide';
+        } else {
+            this.buttonName = 'Show';
+        }
     }
 
 }
